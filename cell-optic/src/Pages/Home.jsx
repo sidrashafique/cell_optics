648ae13079a2881351842f9358e18fe77db28494
sidra sahafiquesid <sidrashafiquepak123@gmail.com>
2023-05-30 22:55:07 +0500

    Changes to be committed: 	new file:   cell-optic/.gitignore 	new file:   cell-optic/package-lock.json 	new file:   cell-optic/package.json 	new file:   cell-optic/public/favicon.ico 	new file:   cell-optic/public/index.html 	new file:   cell-optic/public/logo192.png 	new file:   cell-optic/public/logo512.png 	new file:   cell-optic/public/manifest.json 	new file:   cell-optic/public/robots.txt 	new file:   cell-optic/src/App.css 	new file:   cell-optic/src/App.js 	new file:   cell-optic/src/App.test.js 	new file:   cell-optic/src/Pages/Home.jsx 	new file:   cell-optic/src/components/Header.jsx 	new file:   cell-optic/src/index.css 	new file:   cell-optic/src/index.js 	new file:   cell-optic/src/logo.svg 	new file:   cell-optic/src/reportWebVitals.js 	new file:   cell-optic/src/setupTests.js 	new file:   package-lock.json 	new file:   package.json 	new file:   tailwind.config.js

---
+++ cell-optic/src/Pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Banner from '../components/Banner';
+import HomePortfolio from '../components/HomePortfolio';
+import ServicesBody from '../components/ServicesBody';
+import Testimonials from '../components/Testimonials';
+import Header from '../components/Header';
+const Home = () => {
+    return (
+        <div>
+         <Header/>        
+         <div>hgflidughodrhgk</div>   
+        </div>
+    );
+}
+
+export default Home;
